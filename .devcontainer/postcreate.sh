7b5a65a36194f8740a97391eaa31238d288ca00c
Chad Metcalf <metcalfc@gmail.com>
2024-03-08 10:15:26 -0800

    WIP.

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 npm install -g @devcontainers/cli
+
+go install github.com/swaggo/swag/cmd/swag@v1.16.3
 
 go get -u google.golang.org/protobuf/cmd/protoc-gen-go
 go install google.golang.org/protobuf/cmd/protoc-gen-go
@@ -8,4 +12,7 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
 
 go mod tidy
 
-echo 'alias daytona="go run cmd/daytona/main.go"' >> ~/.zshrc
+# Install Oh My Zsh and lets get a nice prompt
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo 'alias daytona="go run cmd/daytona/main.go"' >> "$HOME/.zshrc"
